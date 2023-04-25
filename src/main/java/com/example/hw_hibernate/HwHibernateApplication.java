6fa73d99d8a221ea25e9e76d469345268e67fba6
Denis Huric <dhuric231@gmail.com>
2023-04-25 21:40:38 +0200

    Complete Homework

--- src/main/java/com/example/hw_hibernate/HwHibernateApplication.java
+++ src/main/java/com/example/hw_hibernate/HwHibernateApplication.java
@@ -1,7 +1,5 @@
 package com.example.hw_hibernate;
 
-import com.example.hw_hibernate.repository.Person;
-import com.example.hw_hibernate.repository.Repository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HwHibernateApplication implements CommandLineRunner{
-    @Autowired
-    Repository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(HwHibernateApplication.class, args);
     }
@@ -19,7 +16,6 @@ public class HwHibernateApplication implements CommandLineRunner{
     @Override
     @Transactional
     public void run(String... args) throws Exception {
-       repository.generatePersons();
     }
 
 
--- src/main/java/com/example/hw_hibernate/controller/Controller.java
+++ src/main/java/com/example/hw_hibernate/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.example.hw_hibernate.controller;
 
-import com.example.hw_hibernate.repository.Person;
 import com.example.hw_hibernate.repository.Repository;
+import com.example.hw_hibernate.repository.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/persons")
@@ -17,8 +18,18 @@ public class Controller {
     Repository repository;
 
     @GetMapping("/by-city")
-    public List<Person> getCity(@RequestParam String city) {
-        return repository.getPersonsByCity(city);
+    public List<Person> getPersonByCity(@RequestParam String city) {
+        return repository.findByCityOfLiving(city);
+    }
+
+    @GetMapping("/by-age")
+    public List<Person> getPersonByAge(@RequestParam int age) {
+        return repository.findByAgeLessThanOrderByAgeDesc(age);
+    }
+
+    @GetMapping("/by-nameAndSurename")
+    public Optional<Person> getPersonByNameAndSurename(@RequestParam String name, @RequestParam String surename){
+        return repository.findByNameAndSurename(name,surename);
     }
 
 
